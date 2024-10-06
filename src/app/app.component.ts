e287d2ee426dd3b8cbcab74ddc817fa1af912e71
Salih Şengönül <salih.sengonul@eppso.com.tr>
2024-10-06 15:28:42 +0300

    Proje olusturuldu ve i18n eklendi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TranslateModule, TranslateService} from '@ngx-translate/core'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CrewApp';
+  translate: TranslateService=inject(TranslateService);
+
+  changeLanguage(language:string){
+    this.translate.use(language);
+  }
+  
 }
